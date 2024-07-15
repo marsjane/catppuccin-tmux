1265613ebd3a709b17199dfb3eb664b34952d2d8
youxing <youxing>
2024-07-15 14:21:46 +0800

    calendar icon

--- status/date_time.sh
+++ status/date_time.sh
@@ -1,6 +1,6 @@
 show_date_time() {
   local index=$1
-  local icon="$(get_tmux_option "@catppuccin_date_time_icon" "󰃰")"
+  local icon="$(get_tmux_option "@catppuccin_date_time_icon" "")"
   local color="$(get_tmux_option "@catppuccin_date_time_color" "$thm_blue")"
   local text="$(get_tmux_option "@catppuccin_date_time_text" "%Y-%m-%d %H:%M")"
 
